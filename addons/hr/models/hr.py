0493e12fb0b20379c19402286dd4c2dca52fd1ce
abrownpurple <reneehsu1028@gmail.com>
2018-12-20 11:20:26 +0800

    增加上传附件

--- addons/hr/models/hr.py
+++ addons/hr/models/hr.py
@@ -380,6 +380,25 @@ class Employee(models.Model):
         copy=False, default=0)
 
     certificate_name = fields.Char('Certificate Name', store=True)
+    attachment_number = fields.Integer(compute='_compute_attachment_number', string='Number of Attachments')
+
+    @api.multi
+    def _compute_attachment_number(self):
+        """附件上传"""
+        attachment_data = self.env['ir.attachment'].read_group(
+            [('res_model', '=', 'hr.employee'), ('res_id', 'in', self.ids)], ['res_id'], ['res_id'])
+        attachment = dict((data['res_id'], data['res_id_count']) for data in attachment_data)
+        for expense in self:
+            expense.attachment_number = attachment.get(expense.id, 0)
+
+    @api.multi
+    def action_get_attachment_view(self):
+        """附件上传动作视图"""
+        self.ensure_one()
+        res = self.env['ir.actions.act_window'].for_xml_id('base', 'action_attachment')
+        res['domain'] = [('res_model', '=', 'hr.employee'), ('res_id', 'in', self.ids)]
+        res['context'] = {'default_res_model': 'hr.employee', 'default_res_id': self.id}
+        return res
 
     @api.constrains('parent_id')
     def _check_parent_id(self):
