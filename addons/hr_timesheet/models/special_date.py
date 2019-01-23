60920f396cad778d4560ed8b1dbe607bc4ff8741
abrownpurple <reneehsu1028@gmail.com>
2019-01-23 16:45:48 +0800

    调试删除特殊日期的伪数据

--- addons/hr_timesheet/models/special_date.py
+++ addons/hr_timesheet/models/special_date.py
@@ -6,6 +6,8 @@
 # @Desc    :
 
 from odoo import models, fields, api
+import logging
+_logger = logging.getLogger(__name__)
 
 class SpecialDate(models.Model):
     _name = 'timesheet.special_date'
@@ -22,3 +24,15 @@ class SpecialDate(models.Model):
         ('date_uniq', 'unique(date)', '特殊日期不能重复'),
 
     ]
+
+
+    @api.model
+    def create(self, values):
+        if values.get('date') and values.get('options')==2:
+            _logger.info("debug speciak all data %s"%self.env['account.analytic.line'].search(
+                [('date', '=', values.get('date')),('is_fake_data','!=',False)]))
+            rst =self.env['account.analytic.line'].search(
+                [('date', '=', values.get('date')),('is_fake_data','!=',False)]).unlink()
+            _logger.info("debug speciak delete rst %s "%rst)
+        result = super(SpecialDate, self).create(values)
+        return result
